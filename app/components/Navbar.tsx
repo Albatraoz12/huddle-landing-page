929dab4c92974fd1f067b685bf94052edef0548e
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2023-07-04 15:06:57 +0200

    create navbar with only logo

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+      <div className='logo'>
+        <img src='/images/logo.svg' alt='logo image' />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
